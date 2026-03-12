88d11799cd76434a3b704937a4de0d82acac47b3
Vahe Sevachyan <vscode13@gmail.com>
2026-03-11 17:43:06 -0700

    updating app homepage details

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link to="/">Web Flix</Link>
       </div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
